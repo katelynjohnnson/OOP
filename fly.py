9d9db8a6fa8959852b1d0f21d7bb846ec01b215b
Katelyn Johnson <katelyn_johnson1@baylor.edu>
2021-09-13 17:17:33 -0500

    initial commit

---
+++ fly.py
@@ -0,0 +1,12 @@
+import InsectClass as i
+
+mosquito = i.Insect()
+housefly = i.Insect()
+
+mosquito.flightl_length()
+
+
+housefly.flight_length()
+
+print("the mosquito can fly up to: ", mosquito.get_miles(),"miles")
+print("the housefly can fly up to: ", housefly.get_miles(),"miles")
